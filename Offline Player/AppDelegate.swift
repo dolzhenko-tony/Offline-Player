4474291f7d7b99be7c7b729fec21baf5a6de783b
Anton Dolzhenko <anton.dolzhenko@onix-systems.com>
2016-08-13 23:13:38 +0300

    Initial Commit

---
+++ Offline Player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Offline Player
+//
+//  Created by Anton Dolzhenko on 13.08.16.
+//  Copyright © 2016 Onix-Systems, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Offline Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Offline Player
+//
+//  Created by Anton Dolzhenko on 13.08.16.
+//  Copyright © 2016 Onix-Systems, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
